f787736029d3bd7e4122b1b8d1df2f6119b83bf4
Tal Dabran <tal.dabran@live.biu.ac.il>
2022-05-11 13:26:11 +0300

    Post request is Ok

--- MaKore/Controllers/ConnectionController.cs
+++ MaKore/Controllers/ConnectionController.cs
@@ -8,147 +8,70 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MaKore.Data;
 using MaKore.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
 
 namespace MaKore.Controllers
 {
-    public class UsersController : Controller
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ConnectionController : Controller
     {
         private readonly MaKoreContext _context;
+        public IConfiguration _configuration;
 
-        public UsersController(MaKoreContext context)
+        public ConnectionController(MaKoreContext context, IConfiguration config)
         {
+            _configuration = config;
             _context = context;
         }
 
+
         // GET: Users
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
-            return View(await _context.User.ToListAsync());
+            return Json(await _context.User.ToListAsync());
         }
 
-        // GET: Users/Details/5
-        public async Task<IActionResult> Details(string id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _context.User
-                .FirstOrDefaultAsync(m => m.Name == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return View(user);
-        }
-
-        // GET: Users/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Users/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Name,Nickname,Password")] User user)
+        //public IActionResult Post(string username, string passowrd)
+        public IActionResult Post([Bind("Name,Nickname,Passowrd")] User user)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Add(user);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(user);
-        }
 
-        // GET: Users/Edit/5
-        public async Task<IActionResult> Edit(string id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            //bool h = MaKore.Controllers.UsersController.IsExistUser(username, password);
 
-            var user = await _context.User.FindAsync(id);
-            if (user == null)
+            //validate username and password
+            if (true)
             {
-                return NotFound();
-            }
-            return View(user);
-        }
+                string username = user.Name;
+                string password = user.Password;
 
-        // POST: Users/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string id, [Bind("Name,Nickname,Password")] User user)
-        {
-            if (id != user.Name)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
+                var claims = new[]
                 {
-                    _context.Update(user);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!UserExists(user.Name))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(user);
-        }
+                    new Claim(JwtRegisteredClaimNames.Sub, _configuration["JWTParams:Subject"]),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Sub,DateTime.UtcNow.ToString()),
+                    new Claim("Name", username)
+                };
 
-        // GET: Users/Delete/5
-        public async Task<IActionResult> Delete(string id)
-        {
-            if (id == null)
+                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWTParams:SecretKey"]));
+                var mac = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+                var token = new JwtSecurityToken(
+                    _configuration["JWTParams:Issuer"],
+                    _configuration["JWTParams:Audience"],
+                    claims,
+                    expires: DateTime.UtcNow.AddMinutes(20),
+                    signingCredentials: mac);
+                return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+            }
+            else
             {
-                return NotFound();
+                return BadRequest();
             }
-
-            var user = await _context.User
-                .FirstOrDefaultAsync(m => m.Name == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return View(user);
-        }
-
-        // POST: Users/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string id)
-        {
-            var user = await _context.User.FindAsync(id);
-            _context.User.Remove(user);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool UserExists(string id)
-        {
-            return _context.User.Any(e => e.Name == id);
         }
     }
 }

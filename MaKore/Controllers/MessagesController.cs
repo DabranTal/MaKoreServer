0d6889edc9bfa4b8d63c710d138d47ea7f9512a1
Coral Kuta <coral,kuta@live,biu.ac.il>
2022-05-19 20:44:20 +0300

    services

--- MaKore/Controllers/MessagesController.cs
+++ MaKore/Controllers/MessagesController.cs
@@ -1,13 +1,8 @@
 ﻿#nullable disable
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 using MaKore.Models;
 using MaKore.JsonClasses;
+using MaKore.Services;
 
 namespace MaKore.Controllers
 {
@@ -17,13 +12,13 @@ namespace MaKore.Controllers
 
     public class MessagesController : BaseController
     {
-        private readonly MaKoreContext _context;
+        public IMessageService _service;
         public IConfiguration _configuration;
 
         public MessagesController(MaKoreContext context, IConfiguration config)
         {
-            _context = context;
             _configuration = config;
+            _service = new MessageService(context);
         }
 
         // GET: contacts/{id}/messages/{id2?}
@@ -37,49 +32,22 @@ namespace MaKore.Controllers
 
             if (id2 != 0)
             {
-                var q = from message in _context.Messages
-                        where message.Id == id2
-                        select message;
-                if (q.Any())
+                JsonMessage m = _service.Get(id, id2);
+                if (m != null)
                 {
-                    Message mess = q.First();
-
-                    bool sent;
-
-                    if (mess.getSender() == id) { sent = true; } else { sent = false; }
-
-                    return Json(new JsonMessage() { Content = mess.getContent(), Created = Message.getTime(), Id = mess.Id, Sent = sent });
+                    return Json(m);
                 }
                 return BadRequest();
             }
 
-
-            var qu = from conversations in _context.Conversations
-                     where conversations.User.UserName == name && conversations.RemoteUser.UserName == id
-                     select conversations.Messages.ToList();
-
-            if (qu.Any())
+            List<JsonMessage> messages = _service.GetAll(name, id);
+            if (messages != null)
             {
-                List<Message> messages = qu.First();
-
-                var messagesList = new List<JsonMessage>();
-
-                foreach (Message message in messages)
-                {
-                    string sender = message.getSender();
-                    string content = message.getContent();
-                    string time = message.Time;
-                    int Id = message.Id;
-
-                    bool sent;
-                    if (sender == name) { sent = true; } else { sent = false; }
-
-                    messagesList.Add(new JsonMessage() { Content = content, Id = Id, Sent = sent, Created = time });
-                }
-                return Json(messagesList);
+                return Json(messages);
             }
             return BadRequest();
         }
+
 
         // POST: contacts/{id}/messages
         [HttpPost]
@@ -90,69 +58,9 @@ namespace MaKore.Controllers
             authHeader = authHeader.Replace("Bearer ", "");
             string username = UserNameFromJWT(authHeader, _configuration);
 
-            var q = from conv in _context.Conversations
-                    where conv.User.UserName == username && conv.RemoteUser.UserName == id
-                    select conv;
-
-            string newContent;
-            newContent = username + "æ" + message.Content;
-
-            string time = Message.getTime();
-
-            if (q.Any())
+            if (_service.Create(username, id, message) == true)
             {
-                Conversation conversation = q.First();
-                Message newMessage = new Message()
-                {
-                    Content = newContent,
-                    Time = time,
-                    ConversationId = conversation.Id
-                };
-
-                if (conversation.Messages == null)
-                {
-                    conversation.Messages = new List<Message>();
-                }
-
-                conversation.Messages.Add(newMessage);
-                _context.Add(newMessage);
-                _context.SaveChanges();
-
-
-                var q2 = from user in _context.Users
-                         where user.UserName == id
-                         select user;
-
-                // the remote user is also our client (our user)
-                if (q2.Any())
-                {
-                    var q3 = from conv in _context.Conversations
-                             where conv.User.UserName == id && conv.RemoteUser.UserName == username
-                             select conv;
-
-                    if (q3.Any())
-                    {
-                        Conversation contraConversation = q3.First();
-                        Message duplicatedMessage = new Message()
-                        {
-                            Content = newContent,
-                            Time = time,
-                            ConversationId = contraConversation.Id
-                        };
-
-                        if (contraConversation.Messages == null)
-                        {
-                            contraConversation.Messages = new List<Message>();
-                        }
-
-                        contraConversation.Messages.Add(duplicatedMessage);
-                        _context.Add(duplicatedMessage);
-                        _context.SaveChanges();
-                    }
-                }
                 return StatusCode(201);
-
-
             }
             return BadRequest();
         }
@@ -167,22 +75,13 @@ namespace MaKore.Controllers
             authHeader = authHeader.Replace("Bearer ", "");
             string name = UserNameFromJWT(authHeader, _configuration);
 
-
-            var q = from mess in _context.Messages
-                    where mess.Id == id2
-                    select mess;
-
-            if (q.Any())
+            if (_service.Edit(id2, message) == true)
             {
-                Message m = q.First();
-                string sender = m.getSender();
-                m.Content = sender + "æ" + message.Content;
-                _context.SaveChanges();
-                // ????????
                 return StatusCode(201);
             }
             return BadRequest();
         }
+
 
         // DELETE: contacts/{id}/messages/{id2}
         [HttpDelete("{id2}")]
@@ -193,21 +92,15 @@ namespace MaKore.Controllers
             authHeader = authHeader.Replace("Bearer ", "");
             string name = UserNameFromJWT(authHeader, _configuration);
 
-            var q = from mess in _context.Messages
-                    where mess.Id == id2
-                    select mess;
 
-            if (q.Any())
+            if (_service.Delete(id2) == true)
             {
-                Message m = q.First();
-                _context.Remove(m);
-                _context.SaveChanges();
-                return NoContent();          //204
+                return NoContent();
             }
             return BadRequest();
-
-
-
         }
+
+
+    
     }
 }
---
+++ MaKore/Services/IConversationService.cs
@@ -0,0 +1,11 @@
+﻿using MaKore.JsonClasses;
+using MaKore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaKore.Services
+{
+    public interface IConversationService
+    {
+        public bool Create(string currUser, RemoteUser remoteUser);
+    }
+}

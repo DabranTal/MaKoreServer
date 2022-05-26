e5e77914599dcc9025923057c3a34dbcb4741f15
idota <IdoTavron7>
2022-05-26 09:41:33 +0300

    merge

--- MaKore/Controllers/InvitationsController.cs
+++ MaKore/Controllers/InvitationsController.cs
@@ -9,6 +9,8 @@ using Microsoft.EntityFrameworkCore;
 using MaKore.Models;
 using MaKore.JsonClasses;
 using MaKore.Services;
+using Microsoft.AspNetCore.SignalR;
+using MaKore.Hubs;
 
 namespace MaKore.Controllers
 {
@@ -19,19 +21,32 @@ namespace MaKore.Controllers
     {
         public IInvitationsService _service;
         public IConfiguration _configuration;
+        public IHubContext<MessagesHub> _hub;
 
-        public InvitationsController(MaKoreContext context, IConfiguration config)
+        public InvitationsController(MaKoreContext context, IConfiguration config, IHubContext<MessagesHub> hub)
         {
             _service = new InvitationsService(context);
             _configuration = config;
+            _hub = hub;
+        }
+
+        public async Task sendFriend(JsonHubChat immediateChat) {
+            await _hub.Clients.Group(immediateChat.userName).SendAsync("ReciveFriend", immediateChat.userName, immediateChat.nickName, immediateChat.remoteUserName);
         }
 
         // a different server serfs here, thus no authantication. Someone wants to talk to OUR user
         [HttpPost("invitations")]
-        public IActionResult Invitations([Bind("From,To,Server")] JsonInvitation info)
+        public async Task<IActionResult> Invitations([Bind("From,To,Server")] JsonInvitation info)
         {
             if (_service.CreateOnInvitation(info) == true)
             {
+                JsonHubChat jsc = new JsonHubChat()
+                {
+                    userName = info.To,
+                    remoteUserName = info.From,
+                    nickName = info.To
+                };
+                await sendFriend(jsc);
                 return StatusCode(201);
             }
             return BadRequest();
